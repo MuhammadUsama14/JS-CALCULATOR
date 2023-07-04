34eea63faa717c89c281dd37827c18cd6780b731
MuhammadUsama14 <125475144+MuhammadUsama14@users.noreply.github.com>
2023-07-04 11:47:55 +0500

    app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+   const display = document.querySelector(".display");
+   const buttons = document.querySelectorAll("button");
+   const specialChars = [ "%" , "*" , "/" , "-" , "+" , "="];
+   let output = "";
+   //define function to calculate based onbutton clicked.
+   const calculate = (btnValue) => {
+    if(btnValue === "=" && output !== "") {
+        // If output has "%", replace with "/100" before evaluating.
+    output = eval(output.replace("%", "/100"));
+    }else if(btnValue === "AC"){
+        output = "";
+    }else if(btnValue === "DEL") {
+        // IF DEL button is clicked remove the last character from output.
+        output = output.toString().slice(0,-1);
+    }else {
+        // If output is empty and button is specialChars then return.
+        if(output === "" && specialChars.includes(btnValue)) return;
+        output += btnValue;
+    }
+    display.value = output;
+   };
+
+
+
+
+
+  // Add Event Listener
+
+  buttons.forEach((button) => {
+
+    button.addEventListener("click" , (e) => calculate(e.target.dataset.value));
+
+  });
